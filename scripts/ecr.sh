808e5e422efc9f7a9d688e9171b85a0b34680826
Muni Annachi <mannachi@amazon.com>
2024-10-03 20:37:48 -0400

    Added docker login fn

--- scripts/ecr.sh
+++ scripts/ecr.sh
@@ -11,6 +11,18 @@ CUSTOM_IMAGE_REPO_NAME=sagemakerkernels-image-repo
 # FUNCTIONS
 # ------------------------------------------------------------------------------
 
+docker_login () {
+    # log into specified ecr repo
+    ecr_repo=$1
+
+    echo -e "\nlogging into repo $  ...\n"
+
+    aws ecr get-login-password \
+        --region $AWS_REGION | docker login \
+            --username AWS \
+            --password-stdin $ecr_repo
+}
+
 build_sagemaker_images () {
 
     echo_banner "Building Research Platform Images" 
